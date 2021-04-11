22d6ccb00e52dd6f602b4a51ea94cc53a8d90b3d
baraaali <https://github.com/baraaali/git-cours.git>
2021-04-12 00:30:26 +0300

    Authentication Controllers have been added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -37,4 +39,19 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-}
+
+
+    protected function authenticated(Request $request, $user)
+    {
+        if (!$user->verified){
+            Auth::logout();
+            return back()->withErrors([
+
+               ['message' =>'You need to verify email first']
+            ]);
+
+        }
+        return redirect()->intended($this->redirectPath());
+    }
+
+ }
